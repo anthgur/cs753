5b6370e7a4382432a1616a732e4eed046b6ebb2c
William Doyle <williamdoyleiii@gmail.com>
2017-08-31 15:56:40 -0400

    started on indexer

---
+++ src/main/kotlin/edu/unh/cs/ir/a1/Indexer.kt
@@ -0,0 +1,12 @@
+package edu.unh.cs.ir.a1
+
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.FSDirectory
+import java.io.File
+
+class Indexer() {
+
+    val indexDir = FSDirectory.open(Path)
+
+
+}
--- src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
+++ src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
@@ -2,5 +2,10 @@ package edu.unh.cs.ir.a1
 
 fun main(args: Array<String>) {
     println("edu.unh.cs.ir.a1 main running...")
+
+    try {
+        println("rebuildIndexes")
+
+    }
 }
 
