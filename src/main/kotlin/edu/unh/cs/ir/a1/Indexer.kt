86983defd1f7d7a750be9df0c7b920ba97dcaee0
Andrew Mitchell <ajx256@wildcats.unh.edu>
2017-09-02 17:31:37 -0400

    Indexing paragraph items

---
+++ src/main/kotlin/edu/unh/cs/ir/a1/Indexer.kt
@@ -0,0 +1,27 @@
+package edu.unh.cs.ir.a1
+
+import edu.unh.cs.treccar.Data
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.StringField
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.store.FSDirectory
+import java.nio.file.Paths
+
+class Indexer
+{
+    private val indexDir = FSDirectory.open(Paths.get("index-directory"))
+    private val config = IndexWriterConfig(StandardAnalyzer())
+    private val indexWriter = IndexWriter(indexDir, config)
+
+    fun indexParagraph(paragraph: Data.Paragraph)
+    {
+        // Create the document for the paragraph object
+        var doc = Document()
+        doc.add(StringField("id", paragraph.paraId, Field.Store.YES))
+        doc.add(StringField("content", paragraph.textOnly, Field.Store.NO))
+        indexWriter.addDocument(doc)
+    }
+}
--- src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
+++ src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
@@ -1,6 +1,22 @@
 package edu.unh.cs.ir.a1
 
+import edu.unh.cs.treccar.read_data.DeserializeData
+import java.io.FileInputStream
+
 fun main(args: Array<String>) {
     println("edu.unh.cs.ir.a1 main running...")
+
+    // Create an indexer
+    var indexer = Indexer()
+
+    // Get paragraphs from the cbor file
+    var stream = FileInputStream(System.getProperty("user.dir") +
+            "/src/main/resources/input/test200/train.test200.cbor.paragraphs")
+    var paragraphs = DeserializeData.iterableParagraphs(stream)
+    for (paragraph in paragraphs)
+    {
+        // Index every paragraph in Lucene
+        indexer.indexParagraph(paragraph)
+    }
 }
 
