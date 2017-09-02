c38b07011c6ef8b383e699693b676650f0500b18
Anthony Urena <anthgur@users.noreply.github.com>
2017-09-02 18:21:34 -0400

    Adds submodule for unh treccar tools and a minimal working search

--- src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
+++ src/main/kotlin/edu/unh/cs/ir/a1/Main.kt
@@ -1,6 +1,52 @@
 package edu.unh.cs.ir.a1
 
-fun main(args: Array<String>) {
-    println("edu.unh.cs.ir.a1 main running...")
+import edu.unh.cs.treccar.read_data.DeserializeData
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.document.TextField
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.store.RAMDirectory
+import org.apache.lucene.util.QueryBuilder
+import java.io.File
+import java.io.FileInputStream
+
+val ID_FIELD = "id"
+val PARAGRAPH_FIELD = "paragraph"
+
+class FileLoader(val fileName: String) {
+    fun load(): File {
+        val uri = this.javaClass.classLoader.getResource(fileName).toURI()
+        return File(uri)
+    }
 }
 
+fun main(args: Array<String>) {
+    val analyzer = StandardAnalyzer()
+    val directory = RAMDirectory()
+    val config = IndexWriterConfig(analyzer)
+    val writer = IndexWriter(directory, config)
+
+    val file = FileLoader("input/test200/train.test200.cbor.paragraphs").load()
+    val stream = FileInputStream(file)
+    DeserializeData.iterParagraphs(stream).forEach { paragraph ->
+        val document = Document()
+        document.add(Field(ID_FIELD, paragraph.paraId, TextField.TYPE_STORED))
+        document.add(Field(PARAGRAPH_FIELD, paragraph.textOnly, TextField.TYPE_STORED))
+        writer.addDocument(document)
+    }
+    writer.close()
+
+    val reader = DirectoryReader.open(directory)
+    val searcher = IndexSearcher(reader)
+    val builder = QueryBuilder(analyzer)
+    val query = builder.createBooleanQuery(PARAGRAPH_FIELD, "power nap benefits")
+    val hits = searcher.search(query, 1000)
+    hits.scoreDocs.forEach { hit ->
+        val doc = searcher.doc(hit.doc)
+        println(doc.get(ID_FIELD) + ": " + doc.get(PARAGRAPH_FIELD))
+    }
+}
