b9eec0b91b5bc7bcdb282d5194e234543c7019e6
William Doyle <williamdoyleiii@gmail.com>
2017-09-19 12:30:32 -0400

    cleaned up the CLI

--- src/main/kotlin/edu/unh/cs/ir/a2/Main.kt
+++ src/main/kotlin/edu/unh/cs/ir/a2/Main.kt
@@ -8,34 +8,50 @@ import org.apache.lucene.search.similarities.SimilarityBase
 import org.apache.lucene.util.QueryBuilder
 import java.io.FileInputStream
 import edu.unh.cs.ir.tools.*
+import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args: Array<String>) {
     println("edu.unh.cs.ir.a2 main running...")
 
+    val luceneDefaultResults: FileWriter
+    val termFrequencyResults: FileWriter
+    val resultsFile: String
+    val qRelFile: String
+
+
     try {
         if (args[0] == "-init") {
+            println("expecting first argument to be paragraph data file path...")
+            println("expecting second argument to be outline data file path...")
             println("Initializing the results files from paragraph file ${args[1]} and outline file ${args[2]}")
+            println("Saving results at ${System.getProperty("user.dir")}")
+            luceneDefaultResults = FileWriter(System.getProperty("user.dir") + "luceneDefault.results")
+            termFrequencyResults = FileWriter(System.getProperty("user.dir") + "termFrequency.results")
+            generateResults(luceneDefaultResults, termFrequencyResults, args)
         } else if (args[0] == "-eval") {
+            println("expecting first argument to be the qrels file path...")
+            println("expecting second argument to be the results file path...")
             println("Evaluating the results files from ${args[2]} with qrels file ${args[1]}")
+            resultsFile = args[1]
+            qRelFile = args[2]
+            performEvaluation(resultsFile, qRelFile)
         }
     } catch (e: Exception) {
-        System.err.println("Requires all arguments to be use")
+        System.err.println("Requires all arguments to be used!")
+        println("usage:")
+        println("-init [paragraphFilePath] [outlinesFilePath] | to generate results")
+        println("-eval [qRelFilePath] [resultsFileFromInitPath] to evaluate the results")
     }
+}
 
+fun performQuery(searchEngine: SearchEngine, parser: QueryBuilder, query: String, numResults: Int,
+                 metaData: List<String>, resultsFile: FileWriter) {
+    searchEngine.performPageQuery(parser.createBooleanQuery(
+            IndexerFields.CONTENT.toString().toLowerCase(), query), numResults, metaData, resultsFile)
+}
 
-    println("expecting first argument to be paragraph data file path...")
-    try { println("using Paragraphs: ${args[0]} Pages: ${args[1]}")} catch(e: Exception) {
-        System.err.println("Did not give data file path...")
-        System.exit(-1)
-    }
-
-    // Create files to be written to for the search engines
-    val luceneDefaultResults = FileWriter(System.getProperty("user.dir") + "luceneDefault.results")
-    val termFrequencyResults = FileWriter(System.getProperty("user.dir") + "termFrequency.results")
-
-    println("Saving results at ${System.getProperty("user.dir")}")
-
+fun generateResults(luceneDefaultResults: FileWriter, termFrequencyResults: FileWriter, args: Array<String>) {
     class freqSimilarity : SimilarityBase() {
         override fun score(stats: BasicStats?, freq: Float, docLen: Float): Float {
             return stats!!.totalTermFreq.toFloat()
@@ -98,12 +114,10 @@ fun main(args: Array<String>) {
 
     termFrequencyResults.close()
     luceneDefaultResults.close()
-
 }
 
-fun performQuery(searchEngine: SearchEngine, parser: QueryBuilder, query: String, numResults: Int,
-                 metaData: List<String>, resultsFile: FileWriter) {
-    searchEngine.performPageQuery(parser.createBooleanQuery(
-            IndexerFields.CONTENT.toString().toLowerCase(), query), numResults, metaData, resultsFile)
+fun performEvaluation(resultsFile: String, qRelFile: String) {
+    val evaluator = Evaluator(DataReader(qRelFile), DataReader(resultsFile))
+    evaluator.printData()
 }
 
--- src/main/kotlin/edu/unh/cs/ir/tools/Evaluator.kt
+++ src/main/kotlin/edu/unh/cs/ir/tools/Evaluator.kt
@@ -6,9 +6,12 @@ class Evaluator(qRelDataReader: DataReader, resultsDataReader: DataReader) {
     private val testResults = resultsDataReader.readResultsFile()
 
     fun calculateRPrecision() {
-        relevantDocuments.forEach{ key, value ->
-            println("Key $key \n \t\t\t $value")
-        }
+        TODO("implement this function")
+    }
+
+    fun printData() {
+        relevantDocuments.forEach { key, value -> println("Key $key \n \t\t\t $value") }
+        testResults.forEach { key, value -> println("Key $key \n \t\t\t $value") }
     }
 
 }
