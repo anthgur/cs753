435235e795d190e4bc7e514cc4dd7b72eaa49890
William Doyle <williamdoyleiii@gmail.com>
2017-09-19 13:59:36 -0400

    starting mean average precision

--- src/main/kotlin/edu/unh/cs/ir/tools/Evaluator.kt
+++ src/main/kotlin/edu/unh/cs/ir/tools/Evaluator.kt
@@ -5,29 +5,38 @@ class Evaluator(qRelDataReader: DataReader, resultsDataReader: DataReader) {
     private val relevantDocuments = qRelDataReader.readQRelFile()
     private val testResults = resultsDataReader.readResultsFile()
 
-    fun calculateRPrecision() : Double {
+    fun calculateRPrecision(): Double {
         val numberOfQueries = testResults.size.toDouble()
-        var sumOfPrecisions = 0.toDouble()
+        var sumOfPrecisions = 0.0
         relevantDocuments.forEach { query, relevantDocList ->
-            var currentPrecisionSum = 0.toDouble()
+            var currentPrecisionSum = 0.0
             var n = relevantDocList.size
             val resultSet = testResults[query]
             if (resultSet != null) {
                 if (resultSet.size < n) {
                     n = resultSet.size
                 }
-                for ((docID) in resultSet.slice(IntRange(0, n-1))) {
+                for ((docID) in resultSet.slice(IntRange(0, n - 1))) {
                     if (relevantDocList.contains(qRelDataEntry(docID, true))) {
-                        currentPrecisionSum += 1.toDouble()
-//                        println("match! $currentPrecisionSum for $query")
+                        currentPrecisionSum += 1.0
                     }
                 }
                 currentPrecisionSum /= relevantDocList.size.toDouble()
-//                println("precision for $query is $currentPrecisionSum")
                 sumOfPrecisions += currentPrecisionSum
             }
         }
         return sumOfPrecisions / numberOfQueries
+    }
+
+    fun calculateMeanAveragePrecision(): Double {
+        val numberOfQueries = testResults.size.toDouble()
+        var sumOfAveragePrecisions = 0.toDouble()
+        testResults.forEach { query, resultsDocList ->
+            var currentAveragePrecision = 0.toDouble()
+//            var n =
+
+        }
+        return sumOfAveragePrecisions / numberOfQueries
     }
 
     fun printData() {
